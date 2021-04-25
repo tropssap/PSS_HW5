2866a0485f15d3c327c5e9e9f10ad1a7424987fb
tropssap <57821127+tropssap@users.noreply.github.com>
2021-04-25 19:52:41 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -25,19 +25,28 @@ int main() {
 
     Car car("TT","1234",2,3,"0");
     driverGateway.addCar(driver,car);
+    cout<<endl;
     adminGateway.lookForCarsInQueue(admin);
+    cout<<endl;
     adminGateway.validateCar(admin,0);
+    cout<<endl;
    adminGateway.blockPassenger(admin,1);
 ////    passengerGateway.createOrder(passenger,"from","to",2);
     adminGateway.unBlockPassenger(admin,1);
     passengerGateway.createOrder(passenger,"from","to",1);
+    cout<<endl;
     passengerGateway.acceptRide(passenger,1);
+    cout<<endl;
     adminGateway.blockDriver(admin,1002);
   adminGateway.unBlockDriver(admin,1002);
     driverGateway.lookForOrders(driver1);
+    cout<<endl;
    driverGateway.acceptOrder(driver1,0);
+    cout<<endl;
    passengerGateway.getCoordinates(passengerFromSecondDevice);
+    cout<<endl;
     driverGateway.endOrder(driver1);
+    cout<<endl;
     passengerGateway.orderHistory(passengerFromSecondDevice);
 
     return 0;
